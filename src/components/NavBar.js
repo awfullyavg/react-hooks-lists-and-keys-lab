28e509cd844c73dfd15f614199cb098aed065f47
Bryant Wiggins <topwigit@gmail.com>
2023-05-24 11:02:25 -0400

    solution

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,11 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  const atags = links.map((link) => (
+    <a key={link}  href={`#${link}`}>{link}</a>
+  ) )
+
+  return <nav>{atags}</nav>;
 }
 
 export default NavBar;
